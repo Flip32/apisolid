062d1ce1511c7f7584f8c0ab4f7e4078074ff7bb
Flip32 <fl1p3r@gmail.com>
2025-03-18 20:23:35 -0300

    verifying jwt

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -3,6 +3,6 @@ import '@fastify/jwt'
 // This is a declaration file for the Fastify JWT plugin (Serve para nao dar erro na tipagem do sub)
 declare module '@fastify/jwt' {
   export interface FastifyJWT {
-    user: { sub: number }
+    user: { sub: string }
   }
 }
