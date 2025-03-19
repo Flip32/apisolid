e1798eed2de48cab0047b3ce64cb40e8c66fe55c
Flip32 <fl1p3r@gmail.com>
2025-03-19 14:56:35 -0300

    got tests initials e2e done

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,5 +1,23 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, expect, beforeAll, afterAll, it } from 'vitest'
 
-test('register', async () => {
-  console.log('Chegou no teste')
+describe('Register', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(201)
+  })
 })
